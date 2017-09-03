b618f8ae4ffe0ddf1006467c4f747bccdc3a7bd1
vincent zheng <vin_zheng@163.com>
2017-09-03 17:47:15 +0800

    提交修改

--- public/images/readme.js
+++ public/images/readme.js
@@ -1,7 +1,7 @@
 /**
  * 用于保存项目所有的图片，每个页面的图片单独建立一个文件夹存放，公共的图片可以放在images目录下
  *
- * @author  Sven Wang
+ * @author  Vincent Zheng
  * @version 1.0
  * @since   2015-09-17
  * @review
--- public/readme.js
+++ public/readme.js
@@ -1,7 +1,7 @@
 /**
  * 用于保存图片或者其他资源
  *
- * @author  Sven Wang
+ * @author  Vincent Zheng
  * @version 1.0
  * @since   2015-09-18
  * @review
